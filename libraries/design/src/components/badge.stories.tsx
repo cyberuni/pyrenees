334bb24902cefe664996ff25f59a473bbb98a817
Homa Wong <hwong@paloaltonetworks.com>
2025-04-29 15:13:06 -0700

    docs: remove extra story

--- libraries/design/src/components/badge.stories.tsx
+++ libraries/design/src/components/badge.stories.tsx
@@ -30,15 +30,3 @@ export const WithIcon: Story = {
 		className: 'pds:bg-red-100 pds:text-red-800 pds:flex pds:items-center',
 	},
 }
-
-export const MultipleBadges: Story = {
-	render() {
-		return (
-			<div className="pds:flex pds:gap-2">
-				<Badge className="pds:bg-blue-500 pds:text-white">Default</Badge>
-				<Badge className="pds:bg-green-100 pds:text-green-800">Success</Badge>
-				<Badge className="pds:bg-red-100 pds:text-red-800">Error</Badge>
-			</div>
-		)
-	},
-}
