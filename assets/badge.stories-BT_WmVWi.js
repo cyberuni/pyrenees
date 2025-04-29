8d80cf8431bf880f1311d1a4327c4d1d9c77b243
unional <unional@users.noreply.github.com>
2025-04-29 22:16:36 +0000

    deploy: 053e138923abd6b1c153a4365b54395516bdeeca

---
+++ assets/badge.stories-BT_WmVWi.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as o}from"./badge-DsBEe7Yc.js";const l={title:"Components/Badge",component:o,parameters:{layout:"centered"}},s={args:{children:"Badge",className:"pds:bg-blue-500 pds:text-white"}},a={args:{children:e.jsxs(e.Fragment,{children:[e.jsx("span",{className:"pds:mr-1",children:"🔥"}),e.jsx("span",{children:"Hot Badge"})]}),className:"pds:bg-red-100 pds:text-red-800 pds:flex pds:items-center"}};var r,t,d;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Badge',
+    className: 'pds:bg-blue-500 pds:text-white'
+  }
+}`,...(d=(t=s.parameters)==null?void 0:t.docs)==null?void 0:d.source}}};var c,n,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: <>
+                <span className="pds:mr-1">🔥</span>
+                <span>Hot Badge</span>
+            </>,
+    className: 'pds:bg-red-100 pds:text-red-800 pds:flex pds:items-center'
+  }
+}`,...(p=(n=a.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const g=["BasicUsage","WithIcon"];export{s as BasicUsage,a as WithIcon,g as __namedExportsOrder,l as default};
---
+++ assets/color.stories-BW3aS3CO.js
@@ -0,0 +1,11 @@
+import{j as c}from"./jsx-runtime-D_zvdyIk.js";const n={parameters:{layout:"centered"}},e={name:"red-100",render(){return c.jsx("div",{className:"pds:h-8 pds:w-8 pds:bg-red-100"})}},r={name:"red-200",render(){return c.jsx("div",{className:"pds:h-8 pds:w-8 pds:bg-red-200"})}};var s,d,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: 'red-100',
+  render() {
+    return <div className="pds:h-8 pds:w-8 pds:bg-red-100" />;
+  }
+}`,...(a=(d=e.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};var o,t,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: 'red-200',
+  render() {
+    return <div className="pds:h-8 pds:w-8 pds:bg-red-200" />;
+  }
+}`,...(p=(t=r.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};const u=["Red100","Red200"];export{e as Red100,r as Red200,u as __namedExportsOrder,n as default};
