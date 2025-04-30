059792d1f77e7c3b92381156f63f2fb892ec9134
unional <unional@users.noreply.github.com>
2025-04-30 23:32:46 +0000

    deploy: 74b1b9c15f06f40846bac8fb4767260ed7ecabe0

---
+++ assets/use-color-theme.stories-C8FdXh2B.js
@@ -0,0 +1,6 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{u as n}from"./use-color-theme-xjtN0Ycl.js";import"./index-D4lIrffr.js";const d={title:"hooks/useColorTheme"},e={render(){const s=n();return m.jsxs("div",{children:["Current theme: ",s]})}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render() {
+    const theme = useColorTheme();
+    return <div>Current theme: {theme}</div>;
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,d as default};
