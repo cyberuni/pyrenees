1c01e91ddd2041b8a1e677a9fc775ec67ec5c475
unional <unional@users.noreply.github.com>
2025-05-01 18:51:23 +0000

    deploy: 8fca5a3638845b875deb14f4f917310ace1b57c1

--- assets/code-badge.stories-DfJg5YiR.js
+++
@@ -1,5 +0,0 @@
-import{C as t}from"./code-badge-CfO5brVj.js";import"./jsx-runtime-D_zvdyIk.js";import"./bundle-mjs-DrwMUFV_.js";import"./badge-9J7kWppb.js";const m={title:"components/CodeBadge",component:t,parameters:{layout:"centered"}},e={args:{children:"Badge Text"}};var r,a,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Badge Text'
-  }
-}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const p=["BasicUsage"];export{e as BasicUsage,p as __namedExportsOrder,m as default};
