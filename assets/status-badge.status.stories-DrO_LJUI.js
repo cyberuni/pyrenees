7cb07a439c0106892f8479051bae943d6d1c784d
unional <unional@users.noreply.github.com>
2025-05-01 20:36:22 +0000

    deploy: a4aee4f1b81a9074a8b8a82c894cdad5a7efa0ce

---
+++ assets/status-badge.status.stories-DrO_LJUI.js
@@ -0,0 +1,29 @@
+import"./app-container-BBBQmT4f.js";import"./badge-9TyooWln.js";import{S as W}from"./status-badge-CqstdAcA.js";import"./jsx-runtime-D_zvdyIk.js";import"./code-badge-BITTmjs9.js";import"./color-badge-DmkAv_GD.js";import"./severity-badge-CPuEifaI.js";import"./index-D4lIrffr.js";import"./index-DEvoek5M.js";import"./index-BV_IynQ4.js";import"./index-DQD4w61r.js";import"./index-DsJinFGm.js";const G={title:"components/StatusBadge/status",component:W,parameters:{layout:"centered"}},r={name:"default = info",args:{}},s={name:"error",args:{status:"error"}},a={name:"warning",args:{status:"warning"}},e={name:"caution",args:{status:"caution"}},t={name:"success",args:{status:"success"}},o={name:"info",args:{status:"info"}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: 'default = info',
+  args: {}
+}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var u,i,p;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  name: 'error',
+  args: {
+    status: 'error'
+  }
+}`,...(p=(i=s.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var d,g,f;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: 'warning',
+  args: {
+    status: 'warning'
+  }
+}`,...(f=(g=a.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var l,S,w;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: 'caution',
+  args: {
+    status: 'caution'
+  }
+}`,...(w=(S=e.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};var E,x,B;t.parameters={...t.parameters,docs:{...(E=t.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  name: 'success',
+  args: {
+    status: 'success'
+  }
+}`,...(B=(x=t.parameters)==null?void 0:x.docs)==null?void 0:B.source}}};var C,D,I;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  name: 'info',
+  args: {
+    status: 'info'
+  }
+}`,...(I=(D=o.parameters)==null?void 0:D.docs)==null?void 0:I.source}}};const H=["Default","ErrorStatus","Warning","Caution","Success","Info"];export{e as Caution,r as Default,s as ErrorStatus,o as Info,t as Success,a as Warning,H as __namedExportsOrder,G as default};
