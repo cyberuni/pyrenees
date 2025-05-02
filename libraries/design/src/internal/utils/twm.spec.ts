ef64a7eb0fd75662dd3cc28557cbdc691eda1341
Homa Wong <hwong@paloaltonetworks.com>
2025-05-02 10:17:03 -0700

    test: remove extra tests

--- libraries/design/src/internal/utils/twm.spec.ts
+++ libraries/design/src/internal/utils/twm.spec.ts
@@ -20,39 +20,7 @@ describe(`${twm.name}`, () => {
 		expect(twm`${c} pds:text-panw`).toEqual('pds:text-panw')
 	})
 
-	it('merges classes of the same category with different prefix v3', () => {
-		expect(twm`pds:opacity-80 epm-opacity-80`).toEqual('epm-opacity-80')
-	})
-
 	it('merges classes of the same category with different prefix', () => {
 		expect(twm`pds:opacity-80 spf:opacity-80`).toEqual('spf:opacity-80')
-	})
-
-	it('merges with prefix with overlap in v3', () => {
-		expect(twm`pds:opacity-80 spf-opacity-80 spf-nav-opacity-80`).toEqual('spf-nav-opacity-80')
-	})
-
-	describe('v3 syntax', () => {
-		it('merge class with pds- prefix', () => {
-			const p2 = 'pds-p-2'
-			expect(twm`pds-p-1 ${p2}`).toEqual('pds-p-2')
-		})
-
-		it('merge semantic class with base classes', () => {
-			const c = 'pds-bg-panw'
-			expect(twm`pds-bg-category-1-shift-light-1 ${c}`).toEqual('pds-bg-panw')
-		})
-		it('merge semantic class with base classes (text)', () => {
-			const c = tw`pds-text-category-1-shift-light-1`
-			expect(twm`${c} pds-text-panw`).toEqual('pds-text-panw')
-		})
-
-		it('merges classes of the same category with different prefix', () => {
-			expect(twm`pds-opacity-80 epm-opacity-80`).toEqual('epm-opacity-80')
-		})
-
-		it('merges with prefix with overlap', () => {
-			expect(twm`pds-opacity-80 spf-opacity-80 spf-nav-opacity-80`).toEqual('spf-nav-opacity-80')
-		})
 	})
 })
