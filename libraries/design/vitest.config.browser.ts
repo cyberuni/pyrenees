0a59548d2a7ad783394660727ab987ac0b3b9d32
unional <homawong@gmail.com>
2025-05-09 01:54:28 -0700

    chore: use @repobuddy/vitest to simplify config

--- libraries/design/vitest.config.browser.ts
+++ libraries/design/vitest.config.browser.ts
@@ -1,8 +1,9 @@
+import { browserTestPreset, mergeConfig } from '@repobuddy/vitest/config'
 import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { storybookVis } from 'storybook-addon-vis/vitest-plugin'
-import { defineConfig, mergeConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 import config from './vite.config.js'
 
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
@@ -11,39 +12,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 export default mergeConfig(
 	config,
 	defineConfig({
-		plugins: [
-			storybookTest({ configDir: path.join(dirname, '.storybook') }),
-			storybookVis(),
-			{
-				name: 'override',
-				config(base) {
-					return mergeConfig(
-						defineConfig({
-							test: {
-								browser: {
-									enabled: true,
-									headless: true,
-									screenshotFailures: false,
-									instances: [
-										{
-											name: `${base.test!.name} (chromium)`,
-											browser: 'chromium',
-										},
-									],
-									provider: 'playwright',
-								},
-								include: [
-									'{src,source,code,tests}/**/*.{spec,test,unit,accept,integrate,system,perf,stress,study}.{js,cjs,mjs,ts,cts,mts}',
-									'{src,source,code,tests}/**/*.{spec,test,unit,accept,integrate,system,perf,stress,study}.{jsx,tsx}',
-									'{src,source,code,tests}/**/*.{spec,test,unit,accept,integrate,system,perf,stress,study}.browser*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
-								],
-							},
-						}),
-						base,
-					)
-				},
-			},
-		],
+		plugins: [storybookTest({ configDir: path.join(dirname, '.storybook') }), storybookVis(), browserTestPreset()],
 		test: {
 			name: 'design',
 			setupFiles: ['vitest.setup.ts'],
