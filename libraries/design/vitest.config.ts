44da37ec35d708b3d662774eb238c9fd9b2c653c
unional <homawong@gmail.com>
2025-05-09 01:54:28 -0700

    chore: use @repobuddy/vitest to simplify config

--- libraries/design/vitest.config.ts
+++ libraries/design/vitest.config.ts
@@ -1,18 +1,14 @@
+import { configDefaults } from '@repobuddy/vitest/config'
 import { defineConfig, mergeConfig } from 'vitest/config'
 import config from './vite.config.js'
 
-// More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default mergeConfig(
 	config,
 	defineConfig({
 		test: {
 			coverage: {
-				include: ['{src,source,code}/**/*.{js,mjs,cjs,ts,jsx,tsx,cts,mts}'],
-				exclude: [
-					'**/*.{spec,test,unit,accept,integrate,system,perf,stress,study}.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
-					'**/*.{spec,test,unit,accept,integrate,system,perf,stress,study}.*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
-					'**/*.stories.{js,mjs,jsx,tsx}',
-				],
+				include: configDefaults.include.vitestDefault,
+				exclude: configDefaults.exclude.test,
 			},
 			workspace: ['vitest.config.browser.ts'],
 		},
