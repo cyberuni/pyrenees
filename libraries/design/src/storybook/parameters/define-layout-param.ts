a9ee1c1b87a644bbb20bd14aa3d0d81029136b57
Homa Wong <hwong@paloaltonetworks.com>
2025-05-21 15:33:41 -0700

    refactor: use defineLayoutParam from repobuddy

--- libraries/design/src/storybook/parameters/define-layout-param.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Defines layout parameters for a Storybook story.
- * @param layout - The layout type to use ('padded', 'centered', or 'fullscreen').
- * @returns An object with the specified layout parameter.
- */
-export function defineLayoutParam(layout: 'padded' | 'centered' | 'fullscreen') {
-	return { layout }
-}
